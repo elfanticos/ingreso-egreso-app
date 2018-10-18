4fe1e14832bfc9dcd6aac0438f1f3fc70a66dec8
jmeza <dart.bekicfox@gmail.com>
2018-10-18 01:18:57 -0500

    commit

--- src/app/ingreso-egreso/estadistica/estadistica.component.ts
+++ src/app/ingreso-egreso/estadistica/estadistica.component.ts
@@ -1,4 +1,8 @@
+import { IngresoEgreso } from './../ingreso-egreso.model';
+import { Subscription } from 'rxjs';
+import { Appstate } from './../../app.reducer';
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-estadistica',
@@ -6,10 +10,36 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class EstadisticaComponent implements OnInit {
-
-  constructor() { }
+  ingresos:number;
+  egresos:number;
+  cuantosIngresos:number;
+  cuantosEgresos:number;
+  subscription = new Subscription();
+  constructor(
+    private _store:Store<Appstate>
+  ) { }
 
   ngOnInit() {
+    this.subscription = this._store.select('ingresoEgreso').subscribe(ingresoEgreso => {
+      this.contarIngresoEgreso(ingresoEgreso.items);
+    });
   }
 
+  contarIngresoEgreso(items:IngresoEgreso[]) {
+    this.ingresos = 0;
+    this.egresos  = 0;
+
+    this.cuantosEgresos  = 0;
+    this.cuantosIngresos = 0;
+
+    items.forEach(item => {
+      if (item.tipo == 'ingreso') {
+        this.cuantosIngresos++;
+        this.ingresos += item.monto;
+      } else {
+        this.cuantosEgresos++;
+        this.egresos += item.monto;
+      }
+    });
+  }
 }
